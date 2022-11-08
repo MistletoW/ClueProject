148e0a7dfcc2f52d7d894acc69dc7f5565210452
MistletoW <haasedylan@gmail.com>
2022-11-07 19:50:41 -0700

    comp AI targeting passes first two requirements

--- src/tests/PlayerTests.java
+++ src/tests/PlayerTests.java
@@ -2,10 +2,14 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import clueGame.Board;
+import clueGame.BoardCell;
 import clueGame.ComputerPlayer;
 import clueGame.Player;
 
@@ -25,6 +29,43 @@ class PlayerTests {
 	}
 	@Test
 	void testCards() {
-		assertEquals(21, board.getDeck().size());
+		assertEquals(0, board.getDeck().size());
+	}
+	
+	@Test
+	void testComputerTargeting() {
+		
+		//create a computerPlayer AI and give a position
+		ComputerPlayer cooper = (ComputerPlayer) board.getPlayers().get(1);
+		cooper.setPosition(17,7);
+		//calcTargets that does not have room and give list to targets
+		board.calcTargets(board.getCell(17, 7), 1);
+		Set<BoardCell> targets = board.getTargets();
+		//get random target for cooper AI
+		BoardCell target = cooper.selectTarget(targets);
+		//create a list of all possible random targets AI could pick
+		ArrayList<BoardCell> testList = new ArrayList<BoardCell>();
+		testList.add(board.getCell(16, 7));
+		testList.add(board.getCell(18, 7));
+		testList.add(board.getCell(17, 6));
+		testList.add(board.getCell(17, 8));
+		//assert that the randomly chosen target cell is in the list of possible targets
+		assertTrue(testList.contains(target));
+		
+		//clear testList
+		testList.clear();
+		//reset cooper to position where two rooms are available
+		cooper.setPosition(4, 25);
+		board.calcTargets(board.getCell(4,25), 2);
+		targets = board.getTargets();
+		target = cooper.selectTarget(targets);
+		//add rooms to testList
+		testList.add(board.getCell(3,21));
+		testList.add(board.getCell(12,28));
+		//assert comp AI choose one of the rooms
+		assertTrue(testList.contains(target));
+		
+		
+
 	}
 }
