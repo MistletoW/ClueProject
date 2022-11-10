835ea8d245fb8f8ff629a03e3b94a66827503ab8
Kathryn Schultz <schultz@mines.edu>
2022-11-10 12:08:55 -0700

    More work done on the game control panel!

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -1,41 +1,47 @@
 package clueGame;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class GameControlPanel extends JPanel{
-	private JPanel mainPanel;
+//	private JPanel mainPanel;
 	public GameControlPanel() {
-		//Big jpanel - gamecontrolpanel
-		
-		
+		createLayout();
 		
 	}
 		public void createLayout() {
-			mainPanel = new JPanel();
 			JPanel subPanel1 = createSubPanel1();
-			JPanel subPanel2 = new JPanel();
-			mainPanel.add(subPanel1);
-			mainPanel.add(subPanel2);
+			JPanel subPanel2 = createSubPanel2();
+			
+			add(subPanel1);
+			add(subPanel2);
+			
 			
 		}
 		
-		public JPanel createSubPanel1() {
-			JPanel whoseTurn = new JPanel();
+		public JPanel createSubPanel1() { 
+			JPanel whoseTurn = new JPanel(new GridLayout(2,0));
 			JLabel prompt = new JLabel("Whose Turn");
 			whoseTurn.add(prompt);
 			JTextField answer = new JTextField(); //note: we will need to change this!
 			whoseTurn.add(answer);
 			
-			JPanel roll = new JPanel();
+			JPanel roll = new JPanel(new GridLayout(0,2));
 			JLabel rollLabel = new JLabel("Roll");
-			roll.add(rollLabel);
+			rollLabel.setLayout(new BorderLayout());
+			roll.add(rollLabel, BorderLayout.CENTER);
 			JTextField rollAnswer = new JTextField();
 			roll.add(rollAnswer);
 			
 			
 			JButton makeAccusation = new JButton("Make Accusation");
+			makeAccusation.setPreferredSize(new Dimension(160,60));
 			JButton next = new JButton("NEXT");
-			JPanel subPanel1 = new JPanel();
+			next.setPreferredSize(new Dimension(180,60));
+			JPanel subPanel1 = new JPanel(new GridLayout(1,4));
 			subPanel1.add(whoseTurn);
 			subPanel1.add(roll);
 			subPanel1.add(makeAccusation);
@@ -45,26 +51,27 @@ public class GameControlPanel extends JPanel{
 		}
 		
 		public JPanel createSubPanel2() {
-			JPanel whoseTurn = new JPanel();
-			JLabel prompt = new JLabel("Whose Turn");
-			whoseTurn.add(prompt);
-			JTextField answer = new JTextField(); //note: we will need to change this!
-			whoseTurn.add(answer);
+			JPanel guessPanel = new JPanel();
+			JLabel guess = new JLabel("Guess");
+			JTextField playerGuess = new JTextField();
+			JPanel resultPanel = new JPanel();
+			JLabel resultLabel = new JLabel("Guess Result");
+			JTextField resultDisplay = new JTextField("So you have nothing?");
 			
-			JPanel roll = new JPanel();
-			JLabel rollLabel = new JLabel("Roll");
-			roll.add(rollLabel);
-			JTextField rollAnswer = new JTextField();
-			roll.add(rollAnswer);
+			//make guess panel
+			guessPanel.add(guess);
+			guessPanel.add(playerGuess);
 			
-			JButton makeAccusation = new JButton("Make Accusation");
-			JButton next = new JButton("NEXT");
-			JPanel subPanel1 = new JPanel();
-			subPanel1.add(whoseTurn);
-			subPanel1.add(roll);
-			subPanel1.add(makeAccusation);
-			subPanel1.add(next);
-			return subPanel1;
+			//make result panel
+			resultPanel.add(resultLabel);
+			resultPanel.add(resultDisplay);
+			
+			//add these to subPanel2
+			
+			JPanel subPanel2 = new JPanel(new GridLayout(0,2));
+			subPanel2.add(guessPanel);
+			subPanel2.add(resultPanel);
+			return subPanel2;
 		}
 		
 	
@@ -82,7 +89,6 @@ public class GameControlPanel extends JPanel{
 		// TODO Auto-generated method stub
 		GameControlPanel panel = new GameControlPanel();
 		JFrame frame = new JFrame();  // create the frame
-		panel.add(panel.createSubPanel1());
 		frame.setContentPane(panel); // put the panel in the frame
 		frame.setSize(750, 180);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
