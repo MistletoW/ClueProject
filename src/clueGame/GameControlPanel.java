97c01d15671ecbbb2a9574d5052b6bb61743b206
MistletoW <haasedylan@gmail.com>
2022-11-11 16:22:31 -0700

    Panel has all buttons and fields

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -1,90 +1,118 @@
 package clueGame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.*;
 
 public class GameControlPanel extends JPanel{
-//	private JPanel mainPanel;
+	//	private JPanel mainPanel;
+	
+	private JPanel subPanel1;
+	private JPanel subPanel2;
+	private JPanel whoseTurn;
+	private JPanel roll;
+	private JPanel guessPanel;
+	private JPanel resultPanel;
+
+	private JLabel prompt;
+	private JLabel rollLabel;
+	private JLabel guess;
+	private JLabel resultLabel;
+	
+	private JTextField answer;
+	private JTextField rollAnswer;
+	private JTextField playerGuess;
+	private JTextField resultDisplay;
+	
+	private JButton makeAccusation;
+	private JButton next;
+	
 	public GameControlPanel() {
 		createLayout();
-		
+
 	}
-		public void createLayout() {
-			JPanel subPanel1 = createSubPanel1();
-			JPanel subPanel2 = createSubPanel2();
-			
-			add(subPanel1);
-			add(subPanel2);
-			
-			
-		}
-		
-		public JPanel createSubPanel1() { 
-			JPanel whoseTurn = new JPanel(new GridLayout(2,0));
-			JLabel prompt = new JLabel("Whose Turn");
-			whoseTurn.add(prompt);
-			JTextField answer = new JTextField(); //note: we will need to change this!
-			whoseTurn.add(answer);
-			
-			JPanel roll = new JPanel(new GridLayout(0,2));
-			JLabel rollLabel = new JLabel("Roll");
-			rollLabel.setLayout(new BorderLayout());
-			roll.add(rollLabel, BorderLayout.CENTER);
-			JTextField rollAnswer = new JTextField();
-			roll.add(rollAnswer);
-			
-			
-			JButton makeAccusation = new JButton("Make Accusation");
-			makeAccusation.setPreferredSize(new Dimension(160,60));
-			JButton next = new JButton("NEXT");
-			next.setPreferredSize(new Dimension(180,60));
-			JPanel subPanel1 = new JPanel(new GridLayout(1,4));
-			subPanel1.add(whoseTurn);
-			subPanel1.add(roll);
-			subPanel1.add(makeAccusation);
-			subPanel1.add(next);
-			
-			return subPanel1;
-		}
-		
-		public JPanel createSubPanel2() {
-			JPanel guessPanel = new JPanel();
-			JLabel guess = new JLabel("Guess");
-			JTextField playerGuess = new JTextField();
-			JPanel resultPanel = new JPanel();
-			JLabel resultLabel = new JLabel("Guess Result");
-			JTextField resultDisplay = new JTextField("So you have nothing?");
-			
-			//make guess panel
-			guessPanel.add(guess);
-			guessPanel.add(playerGuess);
-			
-			//make result panel
-			resultPanel.add(resultLabel);
-			resultPanel.add(resultDisplay);
-			
-			//add these to subPanel2
-			
-			JPanel subPanel2 = new JPanel(new GridLayout(0,2));
-			subPanel2.add(guessPanel);
-			subPanel2.add(resultPanel);
-			return subPanel2;
-		}
-		
-	
-		
+	public void createLayout() {
+		subPanel1 = createSubPanel1();
+		subPanel2 = createSubPanel2();
+
+		add(subPanel1);
+		add(subPanel2);
+
+
+	}
+
+	public JPanel createSubPanel1() { 
+		whoseTurn = new JPanel(new GridLayout(2,0));
+		prompt = new JLabel("Whose Turn");
+		whoseTurn.add(prompt);
+		answer = new JTextField();
+		whoseTurn.add(answer);
+
+		roll = new JPanel();
+		rollLabel = new JLabel("Roll");
+		rollLabel.setLayout(new BorderLayout());
+		roll.add(rollLabel, BorderLayout.CENTER);
+		rollAnswer = new JTextField();
+		rollAnswer.setPreferredSize(new Dimension(60,25));
+		roll.add(rollAnswer);
+
+
+		makeAccusation = new JButton("Make Accusation");
+		makeAccusation.setPreferredSize(new Dimension(160,60));
+		next = new JButton("NEXT");
+		next.setPreferredSize(new Dimension(180,60));
+		JPanel tempSubPanel1 = new JPanel(new GridLayout(1,4));
+		tempSubPanel1.setPreferredSize(new Dimension(720,70));
+		tempSubPanel1.add(whoseTurn);
+		tempSubPanel1.add(roll);
+		tempSubPanel1.add(makeAccusation);
+		tempSubPanel1.add(next);
+
+		return tempSubPanel1;
+	}
+
+	public JPanel createSubPanel2() {
+		guessPanel = new JPanel(new GridLayout(1,0));
+		guessPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
+		guessPanel.setPreferredSize(new Dimension(360,60));
+		guess = new JLabel("Guess");
+		playerGuess = new JTextField("I have no guess!");
+		resultPanel = new JPanel();
+		resultPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
+		resultPanel.setPreferredSize(new Dimension(360,60));
+		resultLabel = new JLabel("Guess Result");
+		resultDisplay = new JTextField("So you have nothing?");
+
+		//make guess panel
+		guessPanel.add(guess);
+		guessPanel.add(playerGuess);
+
+		//make result panel
+		resultPanel.add(resultLabel);
+		resultPanel.add(resultDisplay);
+
+		//add these to subPanel2
+
+		JPanel tempSubPanel2 = new JPanel(new GridLayout(0,2));
+		tempSubPanel2.add(guessPanel);
+		tempSubPanel2.add(resultPanel);
+		return tempSubPanel2;
+	}
+
+
+
 	//two smaller ones inside, stacked on top of each other
-			//within first panel
-				//panel with label and textfield
-				//panel with label and textfield
-				//jbutton1
-				//jbutton2
-			//within second panel
-				//jpanel with text field
-				//jpanel with text field
+	//within first panel
+	//panel with label and textfield
+	//panel with label and textfield
+	//jbutton1
+	//jbutton2
+	//within second panel
+	//jpanel with text field
+	//jpanel with text field
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GameControlPanel panel = new GameControlPanel();
@@ -93,12 +121,12 @@ public class GameControlPanel extends JPanel{
 		frame.setSize(750, 180);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
-		
+
 		// test filling in the data
-//		panel.setTurn(new ComputerPlayer( "Col. Mustard", 0, 0, "orange"), 5);
-//		panel.setGuess( "I have no guess!");
-//		panel.setGuessResult( "So you have nothing?");
-		
+		//		panel.setTurn(new ComputerPlayer( "Col. Mustard", 0, 0, "orange"), 5);
+		//		panel.setGuess( "I have no guess!");
+		//		panel.setGuessResult( "So you have nothing?");
+
 	}
 
 }
