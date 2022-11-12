49cd1127877f5bfa5c43ab93c86f7ccb08c4210d
MistletoW <haasedylan@gmail.com>
2022-11-11 17:07:27 -0700

    setters work, commented code, GUI works

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -10,6 +10,8 @@ import javax.swing.*;
 public class GameControlPanel extends JPanel{
 	//	private JPanel mainPanel;
 	
+	
+	//create all JPanels/Labels/TextFields/Buttons so we can access later
 	private JPanel subPanel1;
 	private JPanel subPanel2;
 	private JPanel whoseTurn;
@@ -30,14 +32,18 @@ public class GameControlPanel extends JPanel{
 	private JButton makeAccusation;
 	private JButton next;
 	
+	//constructor calls createLayout
 	public GameControlPanel() {
 		createLayout();
 
 	}
+	
 	public void createLayout() {
+		//make two subPanels which each have there own items
 		subPanel1 = createSubPanel1();
 		subPanel2 = createSubPanel2();
-
+		
+		//add subpanels to GameControlPanel
 		add(subPanel1);
 		add(subPanel2);
 
@@ -45,12 +51,14 @@ public class GameControlPanel extends JPanel{
 	}
 
 	public JPanel createSubPanel1() { 
+		//create turn panel, give label and text box which contains whose turn it is
 		whoseTurn = new JPanel(new GridLayout(2,0));
 		prompt = new JLabel("Whose Turn");
 		whoseTurn.add(prompt);
 		whoseTurnAnswer = new JTextField();
 		whoseTurn.add(whoseTurnAnswer);
 
+		//create roll panel, give it label and text box for roll
 		roll = new JPanel();
 		rollLabel = new JLabel("Roll");
 		rollLabel.setLayout(new BorderLayout());
@@ -59,11 +67,13 @@ public class GameControlPanel extends JPanel{
 		rollAnswer.setPreferredSize(new Dimension(60,25));
 		roll.add(rollAnswer);
 
-
+		//create accusation panel, give it two buttons, next and make accusation
 		makeAccusation = new JButton("Make Accusation");
 		makeAccusation.setPreferredSize(new Dimension(160,60));
 		next = new JButton("NEXT");
 		next.setPreferredSize(new Dimension(180,60));
+		
+		//create temp panel, add all created panels and return tempPanel
 		JPanel tempSubPanel1 = new JPanel(new GridLayout(1,4));
 		tempSubPanel1.setPreferredSize(new Dimension(720,70));
 		tempSubPanel1.add(whoseTurn);
@@ -75,17 +85,19 @@ public class GameControlPanel extends JPanel{
 	}
 
 	public JPanel createSubPanel2() {
+		//create guess panel, give it guess label and what the player guessed
 		guessPanel = new JPanel(new GridLayout(1,0));
 		guessPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
 		guessPanel.setPreferredSize(new Dimension(360,60));
 		guess = new JLabel("Guess");
 		playerGuess = new JTextField();
+		
+		//create result panel, give it resultlabel and whatever the result is
 		resultPanel = new JPanel(new GridLayout(1,0));
 		resultPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
 		resultPanel.setPreferredSize(new Dimension(360,60));
 		resultLabel = new JLabel("Guess Result");
 		resultDisplay = new JTextField();
-//		resultDisplay.setPreferredSize(new Dimension(200,60));
 
 		//make guess panel
 		guessPanel.add(guess);
@@ -95,7 +107,7 @@ public class GameControlPanel extends JPanel{
 		resultPanel.add(resultLabel);
 		resultPanel.add(resultDisplay);
 
-		//add these to subPanel2
+		//add these to  temp Panel2
 
 		JPanel tempSubPanel2 = new JPanel(new GridLayout(0,2));
 		tempSubPanel2.add(guessPanel);
@@ -104,15 +116,18 @@ public class GameControlPanel extends JPanel{
 	}
 	
 	public void setTurn(Player player, int roll) {
+		//simple setter giving player name and roll value to display
 		whoseTurnAnswer.setText(player.getName());
 		rollAnswer.setText(String.valueOf(roll));
 	}
 	
 	public void setGuess(String guess) {
+		//simple setter giving guess to display
 		playerGuess.setText(guess);
 	}
 	
 	public void setGuessResult(String guess) {
+		//simple setter giving results of guess to display
 		resultDisplay.setText(guess);
 	}
 
@@ -127,7 +142,7 @@ public class GameControlPanel extends JPanel{
 		// test filling in the data
 		panel.setTurn(new ComputerPlayer( "Col. Mustard", 0, 0, "orange"), 5);
 		panel.setGuess( "I have no guess!");
-//		panel.setGuessResult( "So you have nothing?");
+		panel.setGuessResult( "So you have nothing?");
 
 	}
 
