099c1ce6fca3e64cc6d88933dac4ab3bd9f5bf45
MistletoW <haasedylan@gmail.com>
2022-11-16 13:06:14 -0700

    fixed layout issues

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,9 +1,11 @@
 package clueGame;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,16 +14,19 @@ public class ClueGame extends JFrame{
 	public static final int WIDTH = 750;
 	private static Board board;
 	public static void main(String[] args) {
-		JPanel mainPanel = new JPanel(new GridLayout(2,0));
+		JPanel mainPanel = new JPanel(new BorderLayout());
 		board = Board.getInstance();
 		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
 		board.initialize();
 		GameControlPanel gamePanel = new GameControlPanel();
 		KnownCardsPanel cardsPanel = new KnownCardsPanel((board.getPlayers().get(0)));
 		
-		mainPanel.add(board);
-		mainPanel.add(cardsPanel);
-		mainPanel.add(gamePanel);
+//		board.setLayout(new BorderLayout());
+//		cardsPanel.Panel.setLayout(new BorderLayout());
+//		gamePanel.setLayout(new BorderLayout());
+		mainPanel.add(board, BorderLayout.CENTER);
+		mainPanel.add(cardsPanel, BorderLayout.EAST);
+		mainPanel.add(gamePanel, BorderLayout.SOUTH);
 		
 		ClueGame frame = new ClueGame();  // create the frame
 		frame.setContentPane(mainPanel); // put the panel in the frame
